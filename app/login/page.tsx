cd64e587ac565e10833c875aea716ea2bb3012a6
Raheem Kawojue <kawojue08@gmail.com>
2023-08-16 13:56:13 +0100

    [add] login page

---
+++ app/login/page.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client"
+import AuthLayout from '@/components/AuthLayout'
+
+const page = () => {
+    return (
+        <AuthLayout>
+
+        </AuthLayout>
+    )
+}
+
+export default page
