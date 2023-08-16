36abcb04d7459752845c1233f171f2a64210474b
Raheem Kawojue <kawojue08@gmail.com>
2023-08-16 23:03:56 +0100

    ..

--- type.d.ts
+++ type.d.ts
@@ -5,6 +5,15 @@ interface InputProps<T> {
     onChange: (value: T) => void
 }
 
+interface TextEditorStates {
+    isBold: boolean
+    isItalic: boolean
+    isUnderline: boolean
+    setIsBold: (isBold: boolean) => void
+    setIsItalic: (isItalic: boolean) => void
+    setIsUnderline: (isUnderline: boolean) => void
+}
+
 interface UserStoreStates {
     email: string,
     password: string
