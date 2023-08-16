1c3b818d64517442a164ac75fe161f7f9f1eb0da
Raheem Kawojue <kawojue08@gmail.com>
2023-08-16 23:04:23 +0100

    ..

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,12 +1,30 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client"
+import Input from '@/components/Input'
+import { UserStore } from '@/utils/store'
 import AuthLayout from '@/components/AuthLayout'
 
 const page = () => {
+    const {
+        email, setPassword,
+        password, setEmail,
+    } = UserStore()
+
     return (
         <AuthLayout>
-
+            <article className='flex flex-col gap-5'>
+                <Input
+                    type='email'
+                    label='Email'
+                    value={email}
+                    onChange={setEmail} />
+                <Input
+                    type='password'
+                    label='Password'
+                    value={password}
+                    onChange={setPassword} />
+            </article>
         </AuthLayout>
     )
 }
