ed9cd13267325e3c4cfde968062f260369ea9db8
Raheem Kawojue <kawojue08@gmail.com>
2023-08-16 13:54:51 +0100

    [add] notify

---
+++ utils/notify.ts
@@ -0,0 +1,18 @@
+import { toast } from "react-hot-toast"
+
+const notify = (action: string, msg: string) => {
+    if (action === "success") {
+        toast.success(msg, {
+            duration: 1900
+        })
+    }
+
+    if (action === "error") {
+        toast.error(msg, {
+            duration: 2300
+        })
+    }
+}
+
+
+export default notify
