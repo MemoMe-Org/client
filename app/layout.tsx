c0f3b9abe9f22dc9cc2493ab2ea06955889b1136
Raheem Kawojue <kawojue08@gmail.com>
2023-08-16 13:56:43 +0100

    ..

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className="min-h-screen">
         <Toaster
           position="top-center"
           reverseOrder={false} />
