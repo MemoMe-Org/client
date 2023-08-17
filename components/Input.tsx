43aa516523dda2a9716459929ce9d7500d0503e1
Raheem Kawojue <kawojue08@gmail.com>
2023-08-17 11:49:53 +0100

    [edit] input layout

--- components/Input.tsx
+++ components/Input.tsx
@@ -4,8 +4,8 @@ import { poppins } from '@/public/fonts/f'
 import { usePathname } from 'next/navigation'
 
 const Input: FC<InputProps<string>> = ({
-    value, label,
-    type, onChange
+    value, label, type,
+    onChange, placeholder
 }) => {
     const pathName = usePathname()
 
@@ -15,16 +15,19 @@ const Input: FC<InputProps<string>> = ({
                 <label className={`tracking-widest font-medium ${poppins.className}`}>
                     {label}
                 </label>
-                {pathName === "/login" && type === "password" &&
+                {
+                    pathName === "/login" && type === "password" &&
                     <p className="flex justify-between">
                         <Link href='/password/verify'>Forgot Password?</Link>
-                    </p>}
+                    </p>
+                }
             </div>
             <input
                 type={type}
                 value={value}
-                onChange={(e) => onChange(e.target.value)}
-                className="outline-none rounded-full py-1 px-1 text-lg border-[1px] border-clr-8 focus:border-[2px] focus:border-clr-1 trans"
+                placeholder={placeholder}
+                onChange={(e) => onChange(type === "email" ? e.target.value.toLowerCase() : e.target.value)}
+                className="outline-none rounded-full py-1 px-1 text-lg font-medium border-[1px] border-clr-8 focus:border-[2px] focus:border-clr-1 trans"
             />
         </div>
     )
