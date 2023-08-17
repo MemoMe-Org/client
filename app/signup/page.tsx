9fedc9b4b37f4c171f24a895dbddea5a8b6ac0cd
Raheem Kawojue <kawojue08@gmail.com>
2023-08-17 11:45:16 +0100

    more signup

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,20 +1,34 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client"
 import { useState } from 'react'
+import axios from '@/app/api/axios'
+import notify from '@/utils/notify'
 import Input from '@/components/Input'
 import { UserStore } from '@/utils/store'
+import throwError from '@/utils/throwError'
 import AuthLayout from '@/components/AuthLayout'
+import { AxiosResponse, AxiosError } from 'axios'
 
 const page = () => {
     const {
         email, setPassword,
         password, setEmail,
+        resetStates
     } = UserStore()
 
     const [password2, setPassword2] = useState<string>("")
 
+    const handleSignup = async () => {
+        await axios.post('/auth/login', {
+            email, password, password2
+        }).then((res: AxiosResponse) => {
+            resetStates()
+            notify('success', res.data?.msg)
+        }).catch((err: AxiosError) => throwError(err))
+    }
+
     return (
-        <AuthLayout>
+        <AuthLayout handler={handleSignup}>
             <article className='flex flex-col gap-5'>
                 <Input
                     type='email'
