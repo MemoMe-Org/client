43d0f66ff2a437d13969f6f5efd99fbcbbe4ba01
Raheem Kawojue <kawojue08@gmail.com>
2023-08-17 11:44:41 +0100

    more on login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,24 +1,38 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client"
+import axios from '@/app/api/axios'
+import notify from '@/utils/notify'
 import Input from '@/components/Input'
 import { UserStore } from '@/utils/store'
+import throwError from '@/utils/throwError'
 import AuthLayout from '@/components/AuthLayout'
+import { AxiosResponse, AxiosError } from 'axios'
 
 const page = () => {
     const {
-        email, setPassword,
-        password, setEmail,
+        userId, setPassword,
+        password, setUserId,
+        resetStates
     } = UserStore()
 
+    const handleLogin = async () => {
+        await axios.post('/auth/login', { userId, password })
+            .then((res: AxiosResponse) => {
+                resetStates()
+                notify('success', res.data?.msg)
+            })
+            .catch((err: AxiosError) => throwError(err))
+    }
+
     return (
-        <AuthLayout>
+        <AuthLayout handler={handleLogin}>
             <article className='flex flex-col gap-5'>
                 <Input
-                    type='email'
-                    label='Email'
-                    value={email}
-                    onChange={setEmail} />
+                    type='text'
+                    label='Email or Username'
+                    value={userId}
+                    onChange={setUserId} />
                 <Input
                     type='password'
                     label='Password'
