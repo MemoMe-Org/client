5412036985f39104222b479cc3ccd56e0b9f656f
Raheem Kawojue <kawojue08@gmail.com>
2023-08-17 23:40:01 +0100

    [add] use token hook

---
+++ hooks/useToken.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client"
+import { useEffect } from 'react'
+import getCookie from '@/utils/getCookie'
+import { UserStore } from '@/utils/store'
+
+const page = () => {
+    const { token, setToken } = UserStore()
+
+    useEffect(() => {
+        const cookie = getCookie('auth')
+        setToken(cookie)
+    }, [document.cookie])
+
+    return token
+}
+
+export default page
