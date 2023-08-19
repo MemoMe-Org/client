8b166135f3bd4b62721c7a630925d2f118da50f4
Raheem Kawojue <kawojue08@gmail.com>
2023-08-19 22:19:34 +0100

    [add] NavBar

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,6 +3,7 @@
 "use client"
 import axios from '@/app/api/axios'
 import notify from '@/utils/notify'
+import NavBar from '@/components/Nav'
 import Input from '@/components/Input'
 import { UserStore } from '@/utils/store'
 import throwError from '@/utils/throwError'
@@ -31,23 +32,26 @@ const page = () => {
     }
 
     return (
-        <AuthLayout handler={handleLogin} pathName='login'>
-            <article className='flex flex-col gap-5'>
-                <Input
-                    type='text'
-                    label='Email or Username'
-                    value={userId}
-                    onChange={setUserId}
-                />
-                <Input
-                    type='password'
-                    label='Password'
-                    value={password}
-                    placeholder="********"
-                    onChange={setPassword}
-                />
-            </article>
-        </AuthLayout>
+        <>
+            <NavBar isAuthenticated={false} pathName='login' />
+            <AuthLayout handler={handleLogin} pathName='login'>
+                <article className='flex flex-col gap-5'>
+                    <Input
+                        type='text'
+                        label='Email or Username'
+                        value={userId}
+                        onChange={setUserId}
+                    />
+                    <Input
+                        type='password'
+                        label='Password'
+                        value={password}
+                        placeholder="********"
+                        onChange={setPassword}
+                    />
+                </article>
+            </AuthLayout>
+        </>
     )
 }
 
