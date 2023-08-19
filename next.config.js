d71ea767f3af3c3042a683ed5b536eb32def14d5
Raheem Kawojue <kawojue08@gmail.com>
2023-08-19 22:20:49 +0100

    [add] image remote patterns

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+                port: '',
+                pathname: '/kawojue/image/upload/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'd15zb4m4p46ai4.cloudfront.net',
+                port: '',
+                pathname: '/**',
+            },
+        ],
+    },
+}
 
 module.exports = nextConfig
