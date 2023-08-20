5002845408a072ed199c940e477fa75a6a4e4449
Raheem Kawojue <kawojue08@gmail.com>
2023-08-20 02:47:38 +0100

    more on navbar

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,11 +1,26 @@
-import { FC } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { useState } from 'react'
+import axios from '@/app/api/axios'
+import { useState, FC } from 'react'
+import { AxiosResponse } from 'axios'
+import useToken from '@/hooks/useToken'
+import { useRouter } from 'next/navigation'
 import { kaushan, poppins } from '@/public/fonts/f'
+import { FiLogOut, FiSettings, RiAccountCircleFill } from '@/public/icons/ico'
 
 const NavBar: FC<NavProps> = ({ isAuthenticated, pathName, data }) => {
+    const token = useToken()
+    const router = useRouter()
     const [open, setOpen] = useState<boolean>(false)
+
+
+    const logout = async () => {
+        await axios.get('/auth/logout', {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).then((res: AxiosResponse) => router.push('/login'))
+    }
 
     return (
         <header className='fixed top-0 w-full'>
@@ -15,7 +30,7 @@ const NavBar: FC<NavProps> = ({ isAuthenticated, pathName, data }) => {
                     <span className='text-clr-8'>Memo</span>
                     <span className='text-clr-1'>Me</span>
                 </Link>
-                <div>
+                <div className='relative'>
                     {
                         pathName === 'login' &&
                         <Link className='submit-btn' href='/signup'>Sign Up</Link>
@@ -26,7 +41,7 @@ const NavBar: FC<NavProps> = ({ isAuthenticated, pathName, data }) => {
                     }
                     {
                         isAuthenticated &&
-                        <div className='relative'>
+                        <div>
                             <button
                                 onClick={() => setOpen(!open)}
                                 className='relative rounded-full overflow-hidden w-[50px] h-[50px] object-cover border-[0.3125rem] border-clr-5 flex-shrink-0'>
@@ -38,9 +53,28 @@ const NavBar: FC<NavProps> = ({ isAuthenticated, pathName, data }) => {
                                         </div>
                                 }
                             </button>
-                            {
-                                open && <button></button>
-                            }
+                            <ul className={`${open && 'active'} font-medium nav-modal`}>
+                                <li>
+                                    <Link href='/settings' className='flex items-center gap-3 trans hover:text-clr-3'>
+                                        <FiSettings />
+                                        <span>Settings</span>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href='/account' className='flex items-center gap-3 hover:text-clr-3'>
+                                        <RiAccountCircleFill />
+                                        <span>Account</span>
+                                    </Link>
+                                </li>
+                                <li>
+                                    <button onClick={async () => logout()}
+                                        className='flex items-center gap-3 trans hover:text-red-500'>
+                                        <span>Logout</span>
+                                        <FiLogOut />
+                                    </button>
+                                </li>
+
+                            </ul>
                         </div>
                     }
                 </div>
