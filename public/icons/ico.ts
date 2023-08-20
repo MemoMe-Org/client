eaf872ba4d437034c903465e341ced88e3dfbbbd
Raheem Kawojue <kawojue08@gmail.com>
2023-08-20 02:48:46 +0100

    [add] icons

--- public/icons/ico.ts
+++ public/icons/ico.ts
@@ -1,6 +1,11 @@
 import { FcGoogle } from 'react-icons/fc'
 import { AiOutlineGithub } from 'react-icons/ai'
+import { RiAccountCircleFill } from 'react-icons/ri'
+import { FiLogOut, FiSettings } from 'react-icons/fi'
 import { FaUnderline, FaItalic, FaBold } from 'react-icons/fa'
 
 
-export { AiOutlineGithub, FcGoogle, FaUnderline, FaItalic, FaBold }
+export {
+    AiOutlineGithub, FcGoogle, FaUnderline, FaItalic, FaBold,
+    FiLogOut, FiSettings, RiAccountCircleFill
+}
