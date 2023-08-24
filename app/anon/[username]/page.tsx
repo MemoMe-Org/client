88f235f03ff1b907b93c5682c48f9c2c5bcd2471
Raheem Kawojue <kawojue08@gmail.com>
2023-08-24 11:59:36 +0100

    [add] anon-user api

--- app/anon/[username]/page.tsx
+++ app/anon/[username]/page.tsx
@@ -1,10 +1,45 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client"
+import { useEffect } from 'react'
+import axios from '@/app/api/axios'
+import useToken from '@/hooks/useToken'
+import throwError from '@/utils/throwError'
+import { LoaderTwo } from '@/components/Loader'
 import { useQuery } from '@tanstack/react-query'
+import { AxiosError, AxiosResponse } from 'axios'
 
 const page = ({ params: { username } }: Params) => {
-    console.log(username)
+    const token = useToken()
+    const { data, isLoading, refetch } = useQuery({
+        queryKey: ['anon-user'],
+        queryFn: async () => {
+            return await axios.get(
+                `/api/user/anon/${username.toLowerCase().trim()}?token=${token}`
+            )
+                .then((res: AxiosResponse) => {
+                    return res.data
+                }).catch((err: AxiosError) => {
+                    throwError(err)
+                })
+        },
+        enabled: false
+    })
+
+    useEffect(() => {
+        if (token) {
+            refetch()
+        } else {
+            if (token === '') {
+                refetch()
+            }
+        }
+    }, [token])
+
+    if (isLoading) return <LoaderTwo />
+
+    console.log(data)
+
     return (
         <></>
     )
