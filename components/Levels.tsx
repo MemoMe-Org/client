7fa9fa625db17189f9894f1762b202bc963548d3
Raheem Kawojue <kawojue08@gmail.com>
2023-08-25 11:07:10 +0100

    on it

--- components/Levels.tsx
+++ components/Levels.tsx
@@ -3,22 +3,42 @@ import { generativeApi } from '@/app/api/axios'
 
 
 const Levels: FC<LevelProps> = ({ msgPoint, pollPoint }) => {
-    const [data, setData] = useState<any>({})
+    const [data, setData] = useState<any[]>([])
 
-    const overallPoint = (msgPoint + pollPoint) / 2
     const handleLevel = async (level: LevelType, point: number) => {
         const { data } = await generativeApi.get(
             `/levels/${level}?point=${point}`
         )
-
-        setData(data)
+        return data
     }
 
     useEffect(() => {
-        (async () => await handleLevel('message', msgPoint))()
-    }, [msgPoint])
+        const overallPoint = (msgPoint + pollPoint) / 2
+        const tempData: {
+            point: number
+            level: LevelType
+        }[] = [
+                {
+                    point: msgPoint,
+                    level: 'message'
+                },
+                {
+                    point: pollPoint,
+                    level: 'poll'
+                },
+                {
+                    point: overallPoint,
+                    level: 'overall'
+                },
+            ]
 
-    console.log(data)
+        const dataPromises = tempData.map(async (temp) => {
+            return await handleLevel(temp.level, temp.point)
+        });
+
+        (async () => await Promise.all(dataPromises))()
+
+    }, [msgPoint, pollPoint])
 
     return (
         <></>
