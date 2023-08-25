0167566861227f7ae501d7f52044af15256fbe7d
Raheem Kawojue <kawojue08@gmail.com>
2023-08-25 12:57:38 +0100

    ..

---
+++ components/Profile.tsx
@@ -0,0 +1,13 @@
+"use client"
+import { FC } from 'react'
+
+const Profile: FC<IProfile> = ({ user, pathName }) => {
+    console.log(user)
+    return (
+        <main>
+
+        </main>
+    )
+}
+
+export default Profile
