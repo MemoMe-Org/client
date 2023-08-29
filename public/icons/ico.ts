430c7b1a6eacc54f8517bfdf933b4c7373b628cd
Raheem Kawojue <kawojue08@gmail.com>
2023-08-29 11:08:08 +0100

    [add] icons

--- public/icons/ico.ts
+++ public/icons/ico.ts
@@ -1,11 +1,15 @@
-import { AiOutlineGithub } from 'react-icons/ai'
+import { LiaPollHSolid } from 'react-icons/lia'
 import { FcGoogle, FcLock } from 'react-icons/fc'
 import { RiAccountCircleFill } from 'react-icons/ri'
+import { BiSolidMessageDetail } from 'react-icons/bi'
 import { FiLogOut, FiSettings } from 'react-icons/fi'
 import { FaUnderline, FaItalic, FaBold } from 'react-icons/fa'
+import { AiOutlineGithub, AiOutlineCamera, AiOutlinePlus } from 'react-icons/ai'
 
 
 export {
     AiOutlineGithub, FcGoogle, FaUnderline, FaItalic, FaBold,
-    FiLogOut, FiSettings, RiAccountCircleFill, FcLock
+    FiLogOut, FiSettings, RiAccountCircleFill, FcLock,
+    AiOutlineCamera, AiOutlinePlus, LiaPollHSolid,
+    BiSolidMessageDetail,
 }
