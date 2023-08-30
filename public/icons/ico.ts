c6c309bf79a5010646661cb6fad9de206dc457dd
Raheem Kawojue <kawojue08@gmail.com>
2023-08-30 15:02:34 +0100

    ..

--- public/icons/ico.ts
+++ public/icons/ico.ts
@@ -1,3 +1,4 @@
+import { LuVerified } from 'react-icons/lu'
 import { LiaPollHSolid } from 'react-icons/lia'
 import { FcGoogle, FcLock } from 'react-icons/fc'
 import { RiAccountCircleFill } from 'react-icons/ri'
@@ -11,5 +12,5 @@ export {
     AiOutlineGithub, FcGoogle, FaUnderline, FaItalic, FaBold,
     FiLogOut, FiSettings, RiAccountCircleFill, FcLock,
     AiOutlineCamera, AiOutlinePlus, LiaPollHSolid,
-    BiSolidMessageDetail,
+    BiSolidMessageDetail, LuVerified
 }
