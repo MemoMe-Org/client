3e644d9c27eb3039fe7ee50977c115522c517933
Raheem Kawojue <kawojue08@gmail.com>
2023-08-30 15:00:50 +0100

    [add] levels

--- components/Levels.tsx
+++ components/Levels.tsx
@@ -1,5 +1,6 @@
 import { handleLevel } from '@/lib/level'
 import { FC, useEffect, useState } from 'react'
+import { poppins, questrial } from '@/public/fonts/f'
 
 const Levels: FC<LevelProps> = ({ msgPoint, pollPoint }) => {
     const [data, setData] = useState<ILevel[]>([])
@@ -16,52 +17,40 @@ const Levels: FC<LevelProps> = ({ msgPoint, pollPoint }) => {
                 type: 'poll',
                 point: pollPoint,
             },
-        ]
+        ];
 
-        const fetchData = async () => {
-            const dataPromises = tempData.map(async (temp) => {
+        (async (): Promise<void> => {
+            const resolvedData = await Promise.all(tempData.map(async (temp) => {
                 const promisedData = await handleLevel(temp.type, temp.point)
                 const data: ILevel = {
-                    type: temp.type,
-                    point: temp.point,
-                    total: temp.total,
+                    ...temp,
                     level: promisedData?.level
                 }
                 return data
-            })
-            const resolvedData = await Promise.all(dataPromises)
+            }))
             setData(resolvedData)
-        }
-
-        (async () => await fetchData())()
+        })()
     }, [msgPoint, pollPoint])
 
-    console.log(data)
-
     return (
-        <section>
-            {data?.map((level: ILevel, index) => {
-                const innerWidth = (level.point / level.total) * 100
-
-                return (
-                    <article key={index} className='flex flex-col gap-2 mt-5 w-full'>
-                        <div className='flex gap-2 items-center w-full'>
-                            <span>
-                                {level.type}
-                            </span>
-                            :
-                            <div className=' w-full h-8 md:h-5 bg-clr-3 rounded-full overflow-hidden flex items-center justify-between'>
-                                <span className='absolute text-xs w-full'>{level.level}</span>
-                                <span className='absolute text-xs w-full'>{level.point}</span>
-                                <div style={{
-                                    width: `${innerWidth}%`
-                                }} className='bg-clr-10 h-full rounded-full'>
-                                </div>
-                            </div>
-                        </div>
-                    </article>
-                )
-            })}
+        <section className='flex flex-col gap-5 w-full mt-5'>
+            {data?.map(({ type, level, point, total }, index) => (
+                <article key={index} className='flex flex-col gap-0.5'>
+                    <p className='flex gap-1 tracking-wide'>
+                        <span className={`${questrial.className}text-clr-13`}>
+                            {type.charAt(0).toUpperCase() + type.slice(1)}
+                        </span>
+                        <span>&#8226;</span>
+                        <span className={`${poppins.className} text-clr-4 font-medium`}>
+                            {level}
+                        </span>
+                    </p>
+                    <progress
+                        title={`${point}`}
+                        className='level-bar'
+                        value={point} max={total} />
+                </article>
+            ))}
         </section>
     )
 }
