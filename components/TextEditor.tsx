783b7a99ef2c58191032f97eeed1c713fcdfa30c
Raheem Kawojue <kawojue08@gmail.com>
2023-08-31 22:43:07 +0100

    [edit] text editor

--- components/TextEditor.tsx
+++ components/TextEditor.tsx
@@ -15,25 +15,36 @@ const TextEditor: React.FC<{ textEditorRef: any }> = ({ textEditorRef }) => {
     }
 
     return (
-        <section className="rounded-md border-[0.1325rem] border-clr-7 focus:border-clr-4">
-            <div className="border-b-[0.125rem] py-2 flex gap-5 pl-5">
-                <button type="button" title='Bold'
+        <section className='rounded-md border-[0.1325rem] border-clr-7 focus:border-clr-4'>
+            <div className='border-b-[0.125rem] py-2 flex gap-5 pl-5'>
+                <button
+                    type='button'
+                    title='Bold'
                     className={`${isBold && 'active'} editor-btn`}
                     onClick={() => exec('bold', isBold, setIsBold)}>
                     <FaBold />
                 </button>
-                <button type="button" title='Italic'
+                <button
+                    type='button'
+                    title='Italic'
                     className={`${isItalic && 'active'} editor-btn`}
                     onClick={() => exec('italic', isItalic, setIsItalic)}>
                     <FaItalic />
                 </button>
-                <button type="button" title='Underline'
+                <button
+                    type='button'
+                    title='Underline'
                     className={`${isUnderline && 'active'} editor-btn`}
                     onClick={() => exec('underline', isUnderline, setIsUnderline)}>
                     <FaUnderline />
                 </button>
             </div>
-            <div ref={textEditorRef} contentEditable="true" className="editor-box" />
+            <div
+                spellCheck='true'
+                ref={textEditorRef}
+                contentEditable='true'
+                className='editor-box'
+            />
         </section>
     )
 }
