3f4ef8c38c10e11e3d7f04cbfeb214c1ebff9dda
Raheem Kawojue <kawojue08@gmail.com>
2023-09-01 11:00:49 +0100

    [add] text-editor

--- components/TextEditor.tsx
+++ components/TextEditor.tsx
@@ -1,8 +1,14 @@
 "use client"
+import genMsg from '@/lib/genMsg'
+import { FC, useEffect } from 'react'
+import { poppins } from '@/public/fonts/f'
 import { useTextEditor } from '@/utils/store'
 import { FaUnderline, FaItalic, FaBold } from '@/public/icons/ico'
 
-const TextEditor: React.FC<{ textEditorRef: any }> = ({ textEditorRef }) => {
+const TextEditor: FC<{
+    textEditorRef: any,
+    msgType: GenMsgType
+}> = ({ textEditorRef, msgType }) => {
     const {
         isBold, setIsBold,
         isItalic, setIsItalic,
@@ -18,36 +24,61 @@ const TextEditor: React.FC<{ textEditorRef: any }> = ({ textEditorRef }) => {
         set(!get)
     }
 
+    const insertText = async (): Promise<void> => {
+        const insertText = await genMsg(msgType)
+        const contentEditable = textEditorRef.current
+        if (contentEditable && insertText) {
+            contentEditable.textContent = insertText
+        }
+    }
+
+    useEffect(() => {
+        const contentEditable = textEditorRef.current
+        if (contentEditable) {
+            contentEditable.textContent = 'Type your messages here...'
+        }
+    }, [textEditorRef])
+
     return (
-        <section className='rounded-md border-[0.1325rem] border-clr-7 focus:border-clr-4'>
-            <div className='border-b-[0.125rem] py-2 flex gap-5 pl-5'>
+        <section className='rounded-md border-[2px] mt-5'>
+            <div className='border-b-[1px] py-2 flex gap-5 px-6 justify-between'>
+                <div className='flex gap-3 items-center'>
+                    <button
+                        type='button'
+                        title='Bold'
+                        className={`${isBold && 'active'} editor-btn`}
+                        onClick={() => exec('bold', isBold, setIsBold)}>
+                        <FaBold />
+                    </button>
+                    <button
+                        type='button'
+                        title='Italic'
+                        className={`${isItalic && 'active'} editor-btn`}
+                        onClick={() => exec('italic', isItalic, setIsItalic)}>
+                        <FaItalic />
+                    </button>
+                    <button
+                        type='button'
+                        title='Underline'
+                        className={`${isUnderline && 'active'} editor-btn`}
+                        onClick={() => exec('underline', isUnderline, setIsUnderline)}>
+                        <FaUnderline />
+                    </button>
+                </div>
                 <button
-                    type='button'
-                    title='Bold'
-                    className={`${isBold && 'active'} editor-btn`}
-                    onClick={() => exec('bold', isBold, setIsBold)}>
-                    <FaBold />
-                </button>
-                <button
-                    type='button'
-                    title='Italic'
-                    className={`${isItalic && 'active'} editor-btn`}
-                    onClick={() => exec('italic', isItalic, setIsItalic)}>
-                    <FaItalic />
-                </button>
-                <button
-                    type='button'
-                    title='Underline'
-                    className={`${isUnderline && 'active'} editor-btn`}
-                    onClick={() => exec('underline', isUnderline, setIsUnderline)}>
-                    <FaUnderline />
+                    onClick={async () => await insertText()}
+                    className={`${poppins.className} tracking-wider px-2 py-1 rounded-lg bg-clr-4 text-clr-12 hover:bg-clr-5 trans`}
+                    style={{
+                        boxShadow: `rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px`
+                    }}>
+                    Suprise me
                 </button>
             </div>
             <div
                 spellCheck='true'
                 ref={textEditorRef}
                 contentEditable='true'
-                className='editor-box'
+                className={`editor-box ${poppins.className}`}
             />
         </section>
     )
