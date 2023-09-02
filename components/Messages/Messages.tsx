b6ab37f865f6a9fa5ba0e4feb713656fe4397ab4
Raheem Kawojue <kawojue08@gmail.com>
2023-09-03 00:08:40 +0100

    ..

---
+++ components/Messages/Messages.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+const Messages = () => {
+    return (
+        <section>
+            <header className="flex items-center justify-between">
+                <h3>30 Messages</h3>
+                <input
+                    type='text'
+                    className='w-1/3'
+                />
+            </header>
+            <article className="grid w-full">
+
+            </article>
+        </section>
+    )
+}
+
+export default Messages
