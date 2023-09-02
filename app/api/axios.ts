bb407f68d9e6545bb66c5f32bfb62d1afdae9561
Raheem Muyiwa <kawojue08@gmail.com>
2023-09-02 14:11:10 +0100

    Update axios.ts

--- app/api/axios.ts
+++ app/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const baseUrl = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_AUTH_URL : 'http://localhost:2002'
-const generativeUrl = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_AUTH_URL : 'http://localhost:1002'
+const generativeUrl = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_GEN_URL : 'http://localhost:1002'
 
 export default axios.create({
     baseURL: baseUrl,
