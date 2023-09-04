5145e59a802d2a77618a11c69f0e02d25014f656
Raheem Kawojue <kawojue08@gmail.com>
2023-09-04 17:06:56 +0100

    [add] socials

---
+++ components/Modals/Socials.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import Modal from '../Modal'
+
+const Socials: FC<State<boolean>> = ({ get, set }) => {
+    return (
+        <Modal get={get} set={set}>
+
+        </Modal>
+    )
+}
+
+export default Socials
