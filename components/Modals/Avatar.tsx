3dd50b7881f280704e9f567a44408e98b8ee713e
Raheem Kawojue <kawojue08@gmail.com>
2023-09-04 14:44:22 +0100

    .. on it

--- components/Modals/Avatar.tsx
+++ components/Modals/Avatar.tsx
@@ -1,9 +1,17 @@
-import { FC } from 'react'
+/* eslint-disable @next/next/no-img-element */
 import Modal from '../Modal'
+import blob from '@/utils/file'
 import useToken from '@/hooks/useToken'
+import { LoaderThree } from '../Loader'
+import { ChangeEvent, FC, useState } from 'react'
+import { UserStore, useModalStore } from '@/utils/store'
+import { AiOutlineCloudUpload, RiDeleteBin6Line } from '@/public/icons/ico'
 
 const Avatar: FC<ModalComponent> = ({ get, set, data }) => {
     const token = useToken()
+    const { avatar, setAvatar } = UserStore()
+    const { loading, setLoading } = useModalStore()
+    const [avatarPreview, setAvatarPreview] = useState<string>('')
 
     const changeAvatar = async (): Promise<void> => {
 
@@ -13,42 +21,68 @@ const Avatar: FC<ModalComponent> = ({ get, set, data }) => {
 
     }
 
+    const cancel = () => {
+        set(false)
+        setAvatar(null)
+        setAvatarPreview('')
+    }
+
+    const handleFile = (e: ChangeEvent<HTMLInputElement>): void => {
+        blob(e, setAvatarPreview)
+        setAvatar(e.target.files![0])
+    }
+
     return (
         <Modal get={get} set={set} >
-            <form className="modal-form" onSubmit={(e) => e.preventDefault()}>
+            <form
+                className='modal-form'
+                onSubmit={(e) => e.preventDefault()}>
                 <div className='profile-avatar md:w-[12rem] md:h-[12rem] mx-auto'>
                     {avatarPreview ?
-                        <img src={avatarPreview} alt="avatar" /> :
+                        <img src={avatarPreview} alt='avatar' /> :
                         <>
-                            {profile?.avatar?.url ?
-                                <Image src={profile?.avatar?.url}
-                                    width={300} height={300} alt="avatar" loading='lazy' /> :
-                                <Image src="https://res.cloudinary.com/kawojue/image/upload/v1685607626/TOOPCC/Staffs/avatar_ndluis.webp"
-                                    width={300} height={300} alt="avatar" loading='lazy' />
+                            {data?.avatar?.url ?
+                                <img src={data?.avatar?.url}
+                                    width={300} height={300} alt='avatar' loading='lazy' /> :
+                                <img src="https://res.cloudinary.com/kawojue/image/upload/v1685607626/TOOPCC/Staffs/avatar_ndluis.webp"
+                                    width={300} height={300} alt='avatar' loading='lazy' />
                             }
                         </>
                     }
                 </div>
-                <input type="file" accept="image/*" id="avatar"
-                    onChange={(e) => handleFile(e)} className="hidden" />
-                <article className="profile-avatar-btn">
-                    <label htmlFor='avatar' className="change-avatar">
+                <input
+                    type='file'
+                    id='avatar'
+                    accept='image/*'
+                    className='hidden'
+                    onChange={(e) => handleFile(e)}
+                />
+                <article className='profile-avatar-btn'>
+                    <label
+                        htmlFor='avatar'
+                        className='change-avatar'>
                         <AiOutlineCloudUpload />
                         <span>Change photo</span>
                     </label>
-                    <button type='button' className="del-avatar"
+                    <button
+                        type='button'
+                        className='del-avatar'
                         onClick={async () => await delAvatar()}>
                         <RiDeleteBin6Line />
                         <span>Remove photo</span>
                     </button>
                 </article>
             </form>
+
             <div className="modal-btn-container">
-                <button className="save-btn" disabled={!eligible}
-                    type="submit" onClick={async () => await changeAvatar()}>
-                    {loading ? <SpinnerOne /> : 'Save'}
+                <button
+                    className='save-btn' disabled={!Boolean(avatar)}
+                    type='submit'
+                    onClick={async () => await changeAvatar()}>
+                    {loading ? <LoaderThree /> : 'Save'}
                 </button>
-                <button className="cancel-btn"
+                <button
+                    className='cancel-btn'
                     onClick={() => cancel()}>
                     Cancel
                 </button>
