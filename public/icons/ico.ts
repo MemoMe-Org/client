29f1fff6cc9ad6d24d3d04011cc9b21b74a64de9
Raheem Kawojue <kawojue08@gmail.com>
2023-09-04 14:44:01 +0100

    [add] icons

--- public/icons/ico.ts
+++ public/icons/ico.ts
@@ -2,6 +2,7 @@ import { LuVerified } from 'react-icons/lu'
 import { BsFillSendFill } from 'react-icons/bs'
 import { LiaPollHSolid } from 'react-icons/lia'
 import { ImCancelCircle } from 'react-icons/im'
+import { RiDeleteBin6Line } from 'react-icons/ri'
 import { FcGoogle, FcLock } from 'react-icons/fc'
 import { RiAccountCircleFill } from 'react-icons/ri'
 import { BiSolidMessageDetail } from 'react-icons/bi'
@@ -11,9 +12,10 @@ import { AiOutlineGithub, AiOutlineCamera, AiOutlinePlus, AiOutlineCloudUpload }
 
 
 export {
-    AiOutlineGithub, FcGoogle, FaUnderline, FaItalic, FaBold,
     FiLogOut, FiSettings, RiAccountCircleFill, FcLock,
+    AiOutlineGithub, FcGoogle, FaUnderline, FaItalic,
     BiSolidMessageDetail, LuVerified, BsFillSendFill,
     AiOutlineCamera, AiOutlinePlus, LiaPollHSolid,
     AiOutlineCloudUpload, FaPollH, ImCancelCircle,
+    RiDeleteBin6Line, FaBold
 }
