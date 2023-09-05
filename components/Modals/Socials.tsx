67c4e204d047a5d4f2a9a17caa48124c7b5cc9b6
Raheem Kawojue <kawojue08@gmail.com>
2023-09-05 12:10:07 +0100

    .. on it

--- components/Modals/Socials.tsx
+++ components/Modals/Socials.tsx
@@ -1,10 +1,16 @@
 import { FC } from 'react'
 import Modal from '../Modal'
+import Link from 'next/link'
 
 const Socials: FC<State<boolean>> = ({ get, set }) => {
     return (
         <Modal get={get} set={set}>
-
+            <ul className='flex flex-col gap-3'>
+                <li></li>
+                <li>
+                    <Link href={``}></Link>
+                </li>
+            </ul>
         </Modal>
     )
 }
