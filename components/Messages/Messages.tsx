850f67e90035e4065379055e32af838054076d27
Raheem Kawojue <kawojue08@gmail.com>
2023-09-05 14:38:25 +0100

    .. on it

--- components/Messages/Messages.tsx
+++ components/Messages/Messages.tsx
@@ -1,7 +1,56 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client"
-import { FC, useEffect } from 'react'
+import axios from '@/app/api/axios'
+import useToken from '@/hooks/useToken'
+import { FC, useEffect, useState } from 'react'
+import { useMessageStore } from '@/utils/store'
+import { AxiosError, AxiosResponse } from 'axios'
 
 const Messages: FC<TabProps> = ({ username }) => {
+    const limit = 10 as const
+    const {
+        page, setPage,
+    } = useMessageStore()
+    const token = useToken()
+    const [messages, setMessages] = useState<any[]>([])
+
+    const fetchMessages = async (): Promise<void> => {
+        await axios.get(
+            `/api/msg/${username}?limit=${limit}&page=${page}`,
+            {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            }
+        ).then((res: AxiosResponse) => {
+            console.log(res.data)
+            setPage(page + 1)
+            setMessages((prevMessages) => [...prevMessages, ...res.data?.messages])
+        }).catch((res: AxiosError) => {
+
+        })
+    }
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (
+                window.innerHeight + window.scrollY >= document.body.offsetHeight - 100
+            ) {
+                (async () => await fetchMessages())()
+            }
+        }
+
+        window.addEventListener('scroll', handleScroll);
+
+        (async () => await fetchMessages())()
+
+        return () => {
+            window.addEventListener('scroll', handleScroll)
+        }
+    }, [token, page])
+
+    console.log(messages)
+
     return (
         <section>
             <header className="flex items-center justify-between">
