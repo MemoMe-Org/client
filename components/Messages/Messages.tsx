1fd725ecfcbba4e302dfa60af0f1b5997f5f7c4e
Raheem Kawojue <kawojue08@gmail.com>
2023-09-06 11:49:43 +0100

    [fix] messages

--- components/Messages/Messages.tsx
+++ components/Messages/Messages.tsx
@@ -1,20 +1,26 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client"
 import axios from '@/app/api/axios'
+import { AxiosResponse } from 'axios'
 import useToken from '@/hooks/useToken'
 import { FC, useEffect, useState } from 'react'
 import { useMessageStore } from '@/utils/store'
-import { AxiosError, AxiosResponse } from 'axios'
 
 const Messages: FC<TabProps> = ({ username }) => {
     const limit = 10 as const
-    const {
-        page, setPage,
-    } = useMessageStore()
     const token = useToken()
+    const {
+        fetching, setTotalMessages,
+        setFetching, totalMessages,
+    } = useMessageStore()
+    const [page, setPage] = useState<number>(1)
     const [messages, setMessages] = useState<any[]>([])
 
     const fetchMessages = async (): Promise<void> => {
+        if (fetching) {
+            return
+        }
+        setFetching(true)
         await axios.get(
             `/api/msg/${username}?limit=${limit}&page=${page}`,
             {
@@ -23,41 +29,34 @@ const Messages: FC<TabProps> = ({ username }) => {
                 }
             }
         ).then((res: AxiosResponse) => {
-            console.log(res.data)
-            setPage(page + 1)
+            setTotalMessages(res?.data?.length)
             setMessages((prevMessages) => [...prevMessages, ...res.data?.messages])
-        }).catch((res: AxiosError) => {
-
-        })
+        }).finally(() => setFetching(false))
     }
 
     useEffect(() => {
-        const handleScroll = () => {
-            if (
-                window.innerHeight + window.scrollY >= document.body.offsetHeight - 100
-            ) {
+        if (token) {
+            (async () => await fetchMessages())()
+        } else {
+            if (token === '') {
                 (async () => await fetchMessages())()
             }
-        }
-
-        window.addEventListener('scroll', handleScroll);
-
-        (async () => await fetchMessages())()
-
-        return () => {
-            window.addEventListener('scroll', handleScroll)
         }
     }, [token, page])
 
     console.log(messages)
 
     return (
-        <section>
+        <section className='mb-[200px]'>
             <header className="flex items-center justify-between">
                 <h3>30 Messages</h3>
             </header>
             <article className="grid w-full">
-
+                <button
+                    className={`${messages.length === totalMessages && 'hidden'}`}
+                    onClick={() => setPage((prev) => prev + 1)}>
+                    Load more
+                </button>
             </article>
         </section>
     )
