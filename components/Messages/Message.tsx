b98ec629f55cfa20a4d7b27d0ae1e2e052ebbbcf
Raheem Kawojue <kawojue08@gmail.com>
2023-09-07 12:16:01 +0100

    .. on it

--- components/Messages/Message.tsx
+++ components/Messages/Message.tsx
@@ -1,50 +1,99 @@
-import { FC } from 'react'
+import { FC, useRef } from 'react'
 import Image from 'next/image'
 import getPeriod from '@/utils/period'
 import { prompt } from '@/public/fonts/f'
 import { BsDownload } from '@/public/icons/ico'
+import download from '@/utils/download'
+import html2Canvas from 'html2canvas'
 
 const Message: FC<{ message: MessageStates }> = ({ message }) => {
     console.log(message)
 
+    const captureDivRef = useRef<HTMLDivElement>(null)
+
+    const downloadDiv = (id: string) => {
+        const divToCapture = captureDivRef.current
+
+        if (!divToCapture) {
+            return
+        }
+
+        html2Canvas(divToCapture).then((canvas) => {
+            const dataURL = canvas.toDataURL('image/png')
+
+            const downloadLink = document.createElement('a')
+            downloadLink.href = dataURL
+            downloadLink.download = `memome_${id}.png`
+            downloadLink.click()
+        })
+    }
+
     return (
         <>
             <article
-                className={` ${message.files.length === 0 ? 'flex items-center justify-center' : ''} relative w-[300px] min-h-[250px] rounded-[30px]  p-3 bg-clr-11`}
+                className={` ${message.files.length === 0 ? 'flex items-center justify-center' : ''} relative w-[350px] min-h-[250px] rounded-[30px] p-3 bg-clr-11`}
                 style={{
                     boxShadow: `10px 10px 18px 0 rgba(0, 0, 0, 0.3), inset -10px -10px 18px 0 rgba(0, 0, 0, 0.3), inset 10px 10px 18px 0 rgba(255, 255, 255, 0.2)`
                 }}>
                 {message.files.length === 0 ?
-                    <div
-                        className={`${prompt.className} text-[1.2em] text-center text-clr-13`}
-                        dangerouslySetInnerHTML={{ __html: message.texts! }}
-                    /> :
-                    <article className='h-full flex flex-col justify-between'>
+                    <>
                         <div
-                            className={`${prompt.className} text-[1.2em] text-center text-clr-13 border-b-2`}
+                            ref={captureDivRef}
+                            className={`${prompt.className} text- [1.2em] text-center text-clr-13`}
+                            style={{
+                                fontSize: '1.2em',
+                                textAlign: 'center',
+                            }}
                             dangerouslySetInnerHTML={{ __html: message.texts! }}
                         />
-                        {message.files.map((file) => (
-                            <div
-                                key={file.idx}
-                                className='relative w-full flex gap-1 h-full rounded-bl-[30px] rounded-br-[30px] object-cover overflow-hidden'>
-                                {file.type === ('image/png' || 'image/jpeg') ?
-                                    <Image
-                                        priority
-                                        src={file.url}
-                                        width={300}
-                                        height={300}
-                                        alt={file.idx}
-                                    /> : <></>
-                                }
-                                <button className='absolute bottom-2 right-3'>
-                                    <BsDownload />
-                                </button>
-                            </div>
-                        ))}
+                        <button
+                            onClick={() => downloadDiv(message.id)}
+                            className='absolute bottom-3 right-4 text-lg font-semibold text-black'>
+                            <BsDownload />
+                        </button>
+                    </>
+                    :
+                    <article className='h-full flex flex-col justify-between'>
+                        <div
+                            className={`${prompt.className} text-[1.2em] text-center text-clr-13 mt-2`}
+                            dangerouslySetInnerHTML={{ __html: message.texts! }}
+                        />
+                        <div className='flex gap-2 mt-2 h-[200px]'>
+                            {message.files.map((file) => (
+                                <div
+                                    key={file.idx}
+                                    className='relative w-full h-full rounded-bl-[30px] rounded-[30px] object-cover overflow-hidden'>
+                                    {file.type === 'image/png' || file.type === 'image/jpeg' ?
+                                        <Image
+                                            priority
+                                            className='w-full h-full object-cover'
+                                            src={file.url}
+                                            width={300}
+                                            height={300}
+                                            alt={file.idx}
+                                        /> :
+                                        <video
+                                            controls
+                                            width="300"
+                                            height="300"
+                                            className='w-full h-full overflow-hidden object-cover'>
+                                            <source
+                                                src={file.url}
+                                                type={file.type} />
+                                            Your browser does not support the video tag.
+                                        </video>
+                                    }
+                                    <button
+                                        onClick={() => download(file.url)}
+                                        className='absolute bottom-3 right-4 text-lg font-semibold text-black'>
+                                        <BsDownload />
+                                    </button>
+                                </div>
+                            ))}
+                        </div>
                     </article>
                 }
-                <span className={`${message.files.length === 0 ? 'bottom-2' : 'top-2'} absolute right-3 text-xs font-semibold text-clr-4`}>
+                <span className={`absolute top-2 left-3 text-xs font-semibold text-clr-4`}>
                     {getPeriod(message.date)}
                 </span>
             </article>
