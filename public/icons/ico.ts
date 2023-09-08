eed4644ae890061c81cbca9a5f8ad5fc16f2064c
Raheem Kawojue <kawojue08@gmail.com>
2023-09-08 13:05:32 +0100

    [add] icons

--- public/icons/ico.ts
+++ public/icons/ico.ts
@@ -1,21 +1,21 @@
 import { LuVerified } from 'react-icons/lu'
 import { LiaPollHSolid } from 'react-icons/lia'
 import { ImCancelCircle } from 'react-icons/im'
-import { RiDeleteBin6Line } from 'react-icons/ri'
 import { FcGoogle, FcLock } from 'react-icons/fc'
 import { RiAccountCircleFill } from 'react-icons/ri'
+import { MdOutlinePrivacyTip } from 'react-icons/md'
 import { BiSolidMessageDetail } from 'react-icons/bi'
 import { FiLogOut, FiSettings } from 'react-icons/fi'
 import { BsFillSendFill, BsDownload } from 'react-icons/bs'
+import { RiDeleteBin6Line, RiDeleteBin7Line } from 'react-icons/ri'
 import { FaUnderline, FaItalic, FaBold, FaPollH } from 'react-icons/fa'
 import { AiOutlineGithub, AiOutlineCamera, AiOutlinePlus, AiOutlineCloudUpload } from 'react-icons/ai'
 
 
 export {
-    FiLogOut, FiSettings, RiAccountCircleFill, FcLock,
-    AiOutlineGithub, FcGoogle, FaUnderline, FaItalic,
-    BiSolidMessageDetail, LuVerified, BsFillSendFill,
-    AiOutlineCamera, AiOutlinePlus, LiaPollHSolid,
-    AiOutlineCloudUpload, FaPollH, ImCancelCircle,
-    RiDeleteBin6Line, FaBold, BsDownload
+    FiLogOut, FiSettings, RiAccountCircleFill, FcLock, AiOutlineGithub, FcGoogle,
+    FaUnderline, FaItalic, BiSolidMessageDetail, LuVerified, BsFillSendFill,
+    AiOutlineCamera, AiOutlinePlus, LiaPollHSolid, AiOutlineCloudUpload,
+    FaPollH, ImCancelCircle, RiDeleteBin6Line, FaBold, BsDownload,
+    MdOutlinePrivacyTip, RiDeleteBin7Line
 }
