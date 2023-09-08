3074772900feb4d51886218dc45001eb07e0670f
Raheem Kawojue <kawojue08@gmail.com>
2023-09-08 23:52:37 +0100

    .. on it

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,14 +1,43 @@
 "use client"
 import MyPage from '@/components/MyPage'
+import { UserStore } from '@/utils/store'
 
 const page = () => {
+    const { userId, setUserId } = UserStore()
+
     return (
         <MyPage param='account'>
-            {({ data }) => (
-                <section>
-
-                </section>
-            )}
+            {({ data }) => {
+                setUserId(data?.username)
+                return (
+                    <main className='profile'>
+                        <article className="profile-header">
+                            <h1 className='text-2xl text-clr-13 font-semibold tracking-wide md:text-3xl'>
+                                Personal Account
+                            </h1>
+                        </article>
+                        <section>
+                            <article>
+                                <div>
+                                    <h3>Your username</h3>
+                                    <p>This is your account username.</p>
+                                    <div>
+                                        <div>memome.one/</div>
+                                    </div>
+                                    <input
+                                        value={userId}
+                                        onChange={(e) => setUserId(e.target.value)}
+                                    />
+                                </div>
+                                <footer>
+                                    <p>Please use 32 characters at maximum.</p>
+                                    <button>Save</button>
+                                </footer>
+                            </article>
+                        </section>
+                    </main >
+                )
+            }}
         </MyPage>
     )
 }
