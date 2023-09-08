d01e8a1a5c6a132412895f0c2be2a32b21dd0b43
Raheem Kawojue <kawojue08@gmail.com>
2023-09-08 23:26:45 +0100

    refactored

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,66 +1,19 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client"
-import axios from '@/app/api/axios'
-import NavBar from '@/components/Nav'
-import useToken from '@/hooks/useToken'
+import MyPage from '@/components/MyPage'
 import Profile from '@/components/Profile'
-import throwError from '@/utils/throwError'
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/navigation'
-import { LoaderTwo } from '@/components/Loader'
-import { useQuery } from '@tanstack/react-query'
-import { AxiosError, AxiosResponse } from 'axios'
 
 const page = () => {
-    const token = useToken()
-    const router = useRouter()
-
-    const [auth, setAuth] = useState<boolean>(false)
-
-    const { refetch, data, isLoading } = useQuery({
-        queryKey: ['profile'],
-        queryFn: async () => {
-            return await axios.get('/auth/api/profile', {
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
-            }).then((res: AxiosResponse) => {
-                setAuth(true)
-                return res.data?.user || {}
-            }).catch((err: AxiosError) => {
-                const statusCode: unknown = err.response?.status
-                if (statusCode === 401 || statusCode === 403) {
-                    router.push('/login')
-                } else {
-                    throwError(err)
-                }
-            })
-        },
-        enabled: false
-    })
-
-    useEffect(() => {
-        if (token) refetch()
-    }, [token])
-
-    if (isLoading) return <LoaderTwo />
-
-    const username = data?.username
-    const avatar_url = data?.Profile?.avatar?.url
-
     return (
-        <>
-            <NavBar
-                isAuthenticated={auth}
-                data={avatar_url ? { avatar_url } : { username }}
-            />
-
-            <Profile
-                user={data}
-                pathName='main'
-            />
-        </>
+        <MyPage param='profile'>
+            {({ data }) => (
+                <Profile
+                    user={data}
+                    pathName='main'
+                />
+            )}
+        </MyPage>
     )
 }
 
