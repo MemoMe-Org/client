15e2b16dd9d1d784d58973339105f3efd60b0137
Raheem Kawojue <kawojue08@gmail.com>
2023-09-08 23:27:30 +0100

    [add] settings page (on it)

---
+++ app/settings/page.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client"
+import MyPage from '@/components/MyPage'
+
+const page = () => {
+    return (
+        <MyPage param='settings'>
+            {({ data }) => (
+                <section>
+
+                </section>
+            )}
+        </MyPage>
+    )
+}
+
+export default page
