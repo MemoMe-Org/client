50334d793d2a255c66e00a3f2d269795475ee50e
Raheem Kawojue <kawojue08@gmail.com>
2023-09-10 01:25:44 +0100

    [add] token refresher

--- app/api/axios.ts
+++ app/api/axios.ts
@@ -1,9 +1,11 @@
-import axios from 'axios'
+import axiosReq from 'axios'
 
-const baseUrl = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_AUTH_URL : 'http://localhost:2002'
-const generativeUrl = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_GEN_URL : 'http://localhost:1002'
+const isProd = process.env.NODE_ENV === 'production'
 
-export default axios.create({
+const baseUrl = isProd ? process.env.NEXT_PUBLIC_AUTH_URL : 'http://localhost:2002'
+const generativeUrl = isProd ? process.env.NEXT_PUBLIC_GEN_URL : 'http://localhost:1002'
+
+const axios = axiosReq.create({
     baseURL: baseUrl,
     withCredentials: true,
     headers: {
@@ -12,11 +14,33 @@ export default axios.create({
 })
 
 
-const generativeApi = axios.create({
+axios.interceptors.response.use(
+    (response) => {
+        return response
+    },
+    async (err) => {
+        const req = err.config
+
+        if (err.response.status === 401 && err.response.data.message === 'Access token expired.') {
+            try {
+                const res = await axios.post('/auth/refresh')
+                document.cookie = `access_token=${res.data.access_token}; path=/; domain=''; secure; samesite=strict; max-age=${20 * 60}`
+
+                return axios(req)
+            } catch (refreshError) {
+                return Promise.reject(refreshError)
+            }
+        }
+    }
+)
+
+
+const generativeApi = axiosReq.create({
     baseURL: generativeUrl,
     headers: {
         'Content-Type': 'application/json'
     }
 })
 
+export default axios
 export { generativeApi }
