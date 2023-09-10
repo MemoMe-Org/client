0437b3c7a582cbffbb412d5975180207b29cd14e
Raheem Kawojue <kawojue08@gmail.com>
2023-09-10 01:21:06 +0100

    refactored

--- lib/logout.ts
+++ lib/logout.ts
@@ -1,12 +1,9 @@
 import axios from '@/app/api/axios'
 import { AxiosResponse } from 'axios'
 
-const logout = async (token: string) => {
-    return await axios.get('/auth/logout', {
-        headers: {
-            'Authorization': `Bearer ${token}`
-        }
-    }).then((res: AxiosResponse) => res)
+const logout = async () => {
+    return await axios.get('/auth/logout')
+        .then((res: AxiosResponse) => res)
 }
 
 export default logout
