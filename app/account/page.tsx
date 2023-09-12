e17d8b08617e6c351e7579c040abfd12e200aaef
Raheem Kawojue <kawojue08@gmail.com>
2023-09-12 15:50:09 +0100

    ..

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import MyPage from '@/components/MyPage'
+import { inter, poppins, questrial } from '@/public/fonts/f'
 import { UserStore } from '@/utils/store'
 
 const page = () => {
@@ -15,21 +16,27 @@ const page = () => {
                                 Personal Account
                             </h1>
                         </article>
-                        <section>
-                            <article>
-                                <div>
-                                    <h3>Your username</h3>
+                        <section
+                            className={`${poppins.className} flex flex-col gap-10 text-clr-13 `}>
+                            <article className='rounded-lg border-[1px] border-clr-5 overflow-hidden'>
+                                <div className='pt-8 px-6 pb-5'>
+                                    <h3 className={`${inter.className} text-clr-4 font-medium text-xl`}>
+                                        Your username
+                                    </h3>
                                     <p>This is your account username.</p>
-                                    <div>
-                                        <div>memome.one/</div>
+                                    <div className='flex items-center'>
+                                        <div className='bg-clr-14 px-2 py-1 rounded-l-md text-[14px]'>
+                                            memome.one/
+                                        </div>
                                         <input
-                                            placeholder={data?.username}
                                             value={userId}
+                                            placeholder={data?.username}
                                             onChange={(e) => setUserId(e.target.value)}
+                                            className='rrounded-r-md px-2 py-1text-[14px]'
                                         />
                                     </div>
                                 </div>
-                                <footer>
+                                <footer className='px-7 py-2 flex justify-between items-center bg-clr-14 w-full '>
                                     <p>Please use 32 characters at maximum.</p>
                                     <button>Save</button>
                                 </footer>
