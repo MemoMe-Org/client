706a1a1f03c6dec6add91020c3f47e42301d268f
Raheem Kawojue <kawojue08@gmail.com>
2023-09-13 23:25:20 +0100

    .. on it

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -2,12 +2,24 @@
 import MyPage from '@/components/MyPage'
 
 const page = () => {
+
+    // edit bio
+    // show files, allow texts, allow files
+    // edit gen msg type
+
     return (
         <MyPage param='settings'>
             {({ data }) => (
-                <section>
+                <main className='profile'>
+                    <article className="profile-header">
+                        <h1 className='text-2xl text-clr-13 font-semibold tracking-wide md:text-3xl'>
+                            Profile Settings
+                        </h1>
+                    </article>
+                    <section>
 
-                </section>
+                    </section>
+                </main>
             )}
         </MyPage>
     )
