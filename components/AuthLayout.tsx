14d82a60bf4bb54280919151ad63835f63fb8816
Raheem Kawojue <kawojue08@gmail.com>
2023-09-13 10:29:40 +0100

    refcatored

--- components/AuthLayout.tsx
+++ components/AuthLayout.tsx
@@ -7,35 +7,36 @@ import { lato, poppins } from '@/public/fonts/f'
 import { FcGoogle, AiOutlineGithub } from '@/public/icons/ico'
 
 const AuthLayout: FC<AuthProps> = ({
-    children, handler,
-    pathName, title, btnLabel
+    title,
+    handler,
+    children,
+    pathName,
+    btnLabel,
 }) => {
     const { loading } = UserStore()
 
     return (
-        <form onSubmit={(e) => e.preventDefault()}
-            className="card w-[92vw] max-w-[500px] mx-auto mt-3 px-5 py-7">
-            <h2
-                className={`${poppins.className} text-2xl tracking-wider font-medium mb-7 md:text-3xl text-clr-4`}>
+        <form
+            onSubmit={(e) => e.preventDefault()}
+            className="card w-[92vw] max-w-[500px] mx-auto mt-3 mb-10 px-5 py-7">
+            <h2 className={`${poppins.className} text-2xl tracking-wider font-medium mb-7 md:text-3xl text-clr-4`}>
                 {title}
             </h2>
             <article>
                 {children}
                 <div className='flex justify-end mt-3'>
-                    <button onClick={async () => await handler()}
-                        className="submit-btn">
+                    <button
+                        className='submit-btn'
+                        onClick={async () => await handler()}>
                         {loading ? <LoaderThree /> :
                             <>
-                                {
-                                    btnLabel || 'Submit'
-                                }
+                                {btnLabel || 'Submit'}
                             </>
                         }
                     </button>
                 </div>
             </article>
-            {
-                (pathName === 'signup' || pathName === 'login') &&
+            {(pathName === 'signup' || pathName === 'login') &&
                 <>
                     <div className="flex items-center justify-center gap-3">
                         <span className="border-[1.5px] border-clr-x w-full rounded-md" />
@@ -59,17 +60,30 @@ const AuthLayout: FC<AuthProps> = ({
                 </>
             }
             <div className="flex justify-end mt-2 text-[15px]">
-                {
-                    pathName === 'signup' ?
-                        <p>
-                            <span className="mr-2 text-clr-4">Already have an account?</span>
-                            <Link href='/login' className="text-clr-1 hover:text-clr-8 trans font-medium">Login</Link>
-                        </p> :
-                        <p>
-                            <span className="mr-2 text-clr-4">{`Don't have an account?`}</span>
-                            <Link href='/signup' className="text-clr-1 hover:text-clr-8 trans font-medium">Sign Up</Link>
-                        </p>
-                }
+                <p>
+                    {pathName === 'signup' ?
+                        <>
+                            <span className="mr-2 text-clr-4">
+                                Already have an account?
+                            </span>
+                            <Link
+                                href='/login'
+                                className="text-clr-1 hover:text-clr-8 trans font-medium">
+                                Login
+                            </Link>
+                        </> :
+                        <>
+                            <span className="mr-2 text-clr-4">
+                                {`Don't have an account?`}
+                            </span>
+                            <Link
+                                href='/signup'
+                                className="text-clr-1 hover:text-clr-8 trans font-medium">
+                                Sign Up
+                            </Link>
+                        </>
+                    }
+                </p>
             </div>
         </form>
     )
