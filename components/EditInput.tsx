3ffb488d98fba95a5044397f6f4f40620d1a4456
Raheem Kawojue <kawojue08@gmail.com>
2023-09-13 12:59:10 +0100

    [add] input props

--- components/EditInput.tsx
+++ components/EditInput.tsx
@@ -6,15 +6,20 @@ const Input: FC<InputProps<string>> = ({
 }) => {
     return (
         <div className='flex items-center'>
-            <div className='bg-clr-14 px-2 py-1 rounded-l-md text-[14px] border-y-[0.5px] boder-x-[0.5px]'>
+            <div className='bg-clr-14 rounded-l-md text-[12px] md:text-[14px] border-y-[0.5px] boder-x-[0.5px] px-2 py-1 tracking-wide'>
                 {label}
             </div>
             <input
                 type={type}
                 value={value}
+                maxLength={32}
+                autoComplete='off'
+                spellCheck='false'
+                autoCorrect='false'
+                autoCapitalize='false'
                 placeholder={placeholder}
                 onChange={(e) => onChange(e.target.value)}
-                className='text-clr-16 rounded-r-md pl-0.5 py-0.5 text-[15px] outline-none border-y-[0.5px] border-x-[0.5px] focus:border-clr-15'
+                className='text-clr-16 rounded-r-md text-[15px] outline-none border-y-[0.5px] border-x-[0.5px] focus:border-clr-15 px-1 py-0.5'
             />
         </div>
     )
