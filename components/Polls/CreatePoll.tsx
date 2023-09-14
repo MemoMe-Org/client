92cdbff5db29648a8551a2c2ce026d3b3d8e3a09
Raheem Kawojue <kawojue08@gmail.com>
2023-09-14 21:02:21 +0100

    .. on poll creation

---
+++ components/Polls/CreatePoll.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+import Modal from '../Modal'
+
+const CreatePoll: FC<ModalComponent> = ({ get, set, data }) => {
+    return (
+        <Modal
+            get={get}
+            set={set}>
+
+        </Modal>
+    )
+}
+
+export default CreatePoll
