1b4361b0825672d93be2bcace8acc1e1500f922f
Raheem Kawojue <kawojue08@gmail.com>
2023-09-15 01:39:52 +0100

    [add] options funcs

--- components/Polls/CreatePoll.tsx
+++ components/Polls/CreatePoll.tsx
@@ -1,7 +1,36 @@
-import { FC } from 'react'
+import { ChangeEvent, FC } from 'react'
 import Modal from '../Modal'
+import { usePoll } from '@/utils/store'
 
 const CreatePoll: FC<ModalComponent> = ({ get, set, data }) => {
+    const {
+        options, setOptions
+    } = usePoll()
+
+    const handleOptionChange = (
+        id: number,
+        event: ChangeEvent<HTMLInputElement>
+    ) => {
+        const updatedOptions = options.map(
+            (option) => option.id === id ? {
+                ...option,
+                option: event.target.value
+            } : option
+        )
+
+        setOptions(updatedOptions)
+    }
+
+    const handleAddOption = () => {
+        const idx = options.length + 1
+        setOptions([...options, { id: idx, option: '' }])
+    }
+
+    const handleRemoveOption = (id: number) => {
+        const updatedOptions = options.filter((option) => option.id !== id)
+        setOptions(updatedOptions)
+    }
+
     return (
         <Modal
             get={get}
