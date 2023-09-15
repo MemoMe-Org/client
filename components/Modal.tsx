33f83f228b9fb3470a3a5e1f6789f650010bc499
Raheem Kawojue <kawojue08@gmail.com>
2023-09-15 14:49:05 +0100

    ..

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -7,7 +7,7 @@ const Modal: FC<ModalProps> = ({ children, get, set }) => {
     return (
         <>
             <Transition appear show={get} as={Fragment}>
-                <Dialog as="div" className="relative z-10" onClose={() => set(!get)}>
+                <Dialog as="div" className="relative z-[9999999]" onClose={() => set(!get)}>
                     <Transition.Child
                         as={Fragment}
                         enter="ease-out duration-300"
