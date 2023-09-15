be8426baf0c0ba59750b56f02f4d4f7b451a6f87
Raheem Kawojue <kawojue08@gmail.com>
2023-09-15 02:07:25 +0100

    [add] file sizes enum

--- utils/file.ts
+++ utils/file.ts
@@ -1,14 +1,17 @@
 import notify from '@/utils/notify'
 import { ChangeEvent } from 'react'
+import MaxSize from '@/enums/fileMaxSizes'
 
-const checkFile = (file: any): boolean => {
+const checkFile = (
+    file: any,
+    maxSize: MaxSize,
+    ...extensions: string[]
+): boolean => {
     if (!file) return false
-    const maxSize: number = 5_242_880 // 5MB
     const { name, size }: any = file
-    const allowedFormats: string[] = ['jpg', 'png']
     const split: string[] = name.split('.')
     const extension: string = split[split.length - 1]
-    if (allowedFormats.includes(extension) && size <= maxSize) {
+    if (extensions.includes(extension) && size <= maxSize) {
         return true
     }
     return false
@@ -16,11 +19,12 @@ const checkFile = (file: any): boolean => {
 
 const blob = (
     e: ChangeEvent<HTMLInputElement>,
-    set: (get: string) => void
+    set: (get: string) => void,
+    maxSize: MaxSize,
 ): void => {
     const file: any = e.target.files![0]
     const reader: FileReader = new FileReader()
-    if (checkFile(file)) {
+    if (checkFile(file, maxSize)) {
         reader.readAsDataURL(file)
         reader.onload = () => {
             set(reader.result as string)
