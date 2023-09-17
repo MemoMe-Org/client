21481a7b7bf9b41caf9f2df4f9cdcd40493a6f02
Raheem Kawojue <kawojue08@gmail.com>
2023-09-17 23:22:49 +0100

    [edit] max sizes

--- enums/fileMaxSizes.ts
+++ enums/fileMaxSizes.ts
@@ -1,7 +1,6 @@
 enum MaxSize {
     "5MB" = 5_242_880, // avatars
-    "9MB" = 9_437_184, // messages
-    "14MB" = 14_680_064, // polls
+    "9MB" = 9_437_184, // features
 }
 
 export default MaxSize
