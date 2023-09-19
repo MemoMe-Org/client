c2119b8a1eec94cf2704c9df7e22d71997cb0103
Raheem Kawojue <kawojue08@gmail.com>
2023-09-19 12:17:13 +0100

    [add] expiry func

--- utils/period.ts
+++ utils/period.ts
@@ -10,4 +10,31 @@ const getPeriod = (timestamp: string): string => {
     return period
 }
 
-export default getPeriod
+function formatExpiryDate(expiryISOString: string): string {
+    const expiryDate = new Date(expiryISOString)
+    const currentDate = new Date()
+    const timeDifference = expiryDate.getTime() - currentDate.getTime()
+    const timeDifferenceInSeconds = Math.floor(timeDifference / 1000)
+
+    const timeUnits: {
+        unit: string
+        factor: number
+    }[] = [
+            { unit: "year", factor: 31536000 },
+            { unit: "week", factor: 604800 },
+            { unit: "day", factor: 86400 },
+            { unit: "hour", factor: 3600 },
+            { unit: "minute", factor: 60 },
+        ]
+
+    for (const unit of timeUnits) {
+        const unitCount = Math.floor(timeDifferenceInSeconds / unit.factor)
+        if (unitCount >= 1) {
+            return `In ${unitCount} ${unit.unit}${unitCount > 1 ? 's' : ''}`
+        }
+    }
+
+    return "In a few seconds"
+}
+
+export { getPeriod, formatExpiryDate }
