c362c4803847ce6f8fc07c0ead462c0fa6e61c6f
Raheem Kawojue <kawojue08@gmail.com>
2023-09-20 13:51:34 +0100

    refactored

--- components/Polls/OptionsToVote.tsx
+++ components/Polls/OptionsToVote.tsx
@@ -1,17 +1,9 @@
-import axios from '@/app/api/axios'
-import { usePoll } from '@/utils/store'
-import throwError from '@/utils/throwError'
-import { AxiosError, AxiosResponse } from 'axios'
+import { FC } from 'react'
+import OptionToVote from './OptionToVote'
+import { questrial } from '@/public/fonts/f'
 import { formatNumber } from '@/utils/formatNumber'
-import { prompt, questrial } from '@/public/fonts/f'
-import { GiCheckMark, AiOutlineLoading3Quarters } from '@/public/icons/ico'
 
-const OptionsToVote = () => {
-    const {
-        setPoll, voteLoad,
-        poll, setVoteLoad
-    } = usePoll()
-
+const OptionsToVote: FC<{ poll: MyPoll | undefined }> = ({ poll }) => {
     const expired = () => {
         const now = new Date().getTime()
 
@@ -26,15 +18,6 @@ const OptionsToVote = () => {
         return false
     }
 
-    const vote = async (optionId: string) => {
-        setVoteLoad(true)
-        await axios.post(
-            `/api/poll/vote/${poll?.createdById}/${poll?.id}/${optionId}`
-        ).then((res: AxiosResponse) => setPoll(res.data?.poll))
-            .catch((err: AxiosError) => throwError(err))
-            .finally(() => setVoteLoad(false))
-    }
-
     const notValidToVote = (poll?.hasVoted || expired() || poll?.active === false)
 
     return (
@@ -47,40 +30,13 @@ const OptionsToVote = () => {
                             key={option.id}
                             className='w-full'
                         >
-                            <button
-                                onClick={async () => vote(option.id)}
-                                className={`${prompt.className} ${notValidToVote ? 'rounded-md' : 'rounded-full'} relative h-[30px] flex items-center overflow-hidden bg-clr-1 w-full text-clr-0 disabled:bg-clr-9`}
-                                style={{
-                                    boxShadow: `rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px`
-                                }}
-                                disabled={Boolean(poll?.active === false || expired() || poll.hasVoted)}>
-                                {notValidToVote &&
-                                    <div
-                                        className={`transition-all ease-in-out duration-500 absolute bg-clr-3 h-full ${notValidToVote ? 'rounded-md' : 'rounded-full'}`}
-                                        style={{
-                                            width: `${optionPercentage}%`,
-                                            boxShadow: `rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px`
-                                        }} />}
-                                <>
-                                    {voteLoad ?
-                                        <div className='w-full px-2 py-1 flex justify-between relative text-sm'>
-                                            <span>
-                                                {option.texts}
-                                            </span>
-                                            <AiOutlineLoading3Quarters className='vote-load font-bold text-lg' />
-                                        </div> :
-                                        <div className='w-full px-2 py-1 flex justify-between relative text-sm'>
-                                            <span>
-                                                {option.texts}
-                                            </span>
-                                            {(poll.hasVoted && poll.votedOption === option.id) && <GiCheckMark />}
-                                            {notValidToVote &&
-                                                <span>
-                                                    {option.totalVotes} &#8226; {optionPercentage.toFixed(1)}%
-                                                </span>}
-                                        </div>}
-                                </>
-                            </button>
+                            <OptionToVote
+                                expired={expired}
+                                option={option}
+                                notValidToVote={notValidToVote}
+                                optionPercentage={optionPercentage}
+                                poll={poll}
+                            />
                         </article>
                     )
                 })}
