d4eed89a261ef5a8b196b21c0ca3ee5546df5a4e
Raheem Kawojue <kawojue08@gmail.com>
2023-09-20 12:01:47 +0100

    phew!

--- components/Polls/OptionsToVote.tsx
+++ components/Polls/OptionsToVote.tsx
@@ -1,10 +1,10 @@
 import axios from '@/app/api/axios'
 import { usePoll } from '@/utils/store'
-import { prompt, questrial } from '@/public/fonts/f'
 import throwError from '@/utils/throwError'
 import { AxiosError, AxiosResponse } from 'axios'
-import { GiCheckMark } from 'react-icons/gi'
 import { formatNumber } from '@/utils/formatNumber'
+import { prompt, questrial } from '@/public/fonts/f'
+import { GiCheckMark, AiOutlineLoading3Quarters } from '@/public/icons/ico'
 
 const OptionsToVote = () => {
 
@@ -30,11 +30,12 @@ const OptionsToVote = () => {
     }
 
     const vote = async (optionId: string) => {
-        await axios.post(`/api/poll/vote/${poll?.createdById}/${poll?.id}/${optionId}`)
-            .then((res: AxiosResponse) => {
-                console.log(res.data)
-                setPoll(res.data?.poll)
-            }).catch((err: AxiosError) => throwError(err)).finally(() => { })
+        setVoteLoad(true)
+        await axios.post(
+            `/api/poll/vote/${poll?.createdById}/${poll?.id}/${optionId}`
+        ).then((res: AxiosResponse) => setPoll(res.data?.poll))
+            .catch((err: AxiosError) => throwError(err))
+            .finally(() => setVoteLoad(false))
     }
 
 
@@ -44,27 +45,51 @@ const OptionsToVote = () => {
                 {poll?.options?.map((option) => {
                     const optionPercentage = (option.totalVotes / poll.totalVotes) * 100
                     return (
-                        <article key={option.id}
+                        <article
+                            key={option.id}
                             className='w-full'
                         >
                             <button
-                                title={`${optionPercentage.toFixed(1)}%`}
                                 onClick={async () => vote(option.id)}
-                                className={`${prompt.className} ${poll.hasVoted ? 'rounded-md' : 'rounded-full'} px-2 py-1 w-full flex justify-between relative text-sm bg-clr-7 text-clr-0 disabled:bg-clr-9`}
+                                className={`${prompt.className} ${poll.hasVoted ? 'rounded-md' : 'rounded-full'} relative h-[30px] flex items-center overflow-hidden bg-clr-1 w-full text-clr-0 disabled:bg-clr-9`}
                                 style={{
                                     boxShadow: `rgba(0, 0, 0, 0.1) 0px 0px 5px 0px, rgba(0, 0, 0, 0.1) 0px 0px 1px 0px`
                                 }}
                                 disabled={Boolean(poll.active === false || expired() || poll.hasVoted)}>
-                                <span>{option.texts}</span>
-                                {poll.hasVoted && poll.votedOption === option.id && <GiCheckMark />}
-                                {poll.hasVoted && <span>{option.totalVotes}</span>}
+                                {(poll.hasVoted || expired() || poll.active === false) &&
+                                    <div
+                                        className={`transition-all ease-in-out duration-500 absolute bg-clr-3 h-full rounded-md`}
+                                        style={{
+                                            width: `${optionPercentage}%`,
+                                            boxShadow: `rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px`
+                                        }} />}
+                                <>
+                                    {voteLoad ?
+                                        <div className='w-full px-2 py-1 flex justify-between relative text-sm'>
+                                            <span>
+                                                {option.texts}
+                                            </span>
+                                            <AiOutlineLoading3Quarters className='vote-load font-bold text-lg' />
+                                        </div> :
+                                        <div className='w-full px-2 py-1 flex justify-between relative text-sm'>
+                                            <span>
+                                                {option.texts}
+                                            </span>
+                                            {poll.hasVoted && poll.votedOption === option.id && <GiCheckMark />}
+                                            {poll.hasVoted &&
+                                                <span>
+                                                    {option.totalVotes} &#8226; {optionPercentage.toFixed(1)}%
+                                                </span>}
+                                        </div>}
+                                </>
                             </button>
                         </article>
                     )
                 })}
             </div>
-            <div className={`${questrial.className} flex items-center justify-between text-sm text-clr-4 font-medium tracking-wide px-3 md:px-5`}>
+            <div className={`${questrial.className} flex items-center justify-between text-sm text-clr-4 font-medium tracking-wide px-1.5 md:px-3.5`}>
                 <span>Views &#8226; {formatNumber(poll?.views!)}</span>
+                {poll?.active === false || expired() && <span>Final Result</span>}
                 <span>Total Vote(s) &#8226; {poll?.totalVotes}</span>
             </div>
         </section>
