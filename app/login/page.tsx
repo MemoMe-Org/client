d048a8138ccf7d80b826a44ca57b1fdabed5e625
Raheem Kawojue <kawojue08@gmail.com>
2023-09-21 10:50:52 +0100

    refactored

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,63 +1,21 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client"
-import notify from '@/utils/notify'
 import NavBar from '@/components/Nav'
-import Input from '@/components/Input'
-import { axiosReq } from '@/app/api/axios'
-import { UserStore } from '@/utils/store'
-import throwError from '@/utils/throwError'
-import { useRouter } from 'next/navigation'
-import AuthLayout from '@/components/AuthLayout'
-import { AxiosResponse, AxiosError } from 'axios'
+import Login from '@/components/Modals/Login'
 
 const page = () => {
-    const router = useRouter()
-    const {
-        resetStates, setLoading, userId,
-        setPassword, password, setUserId,
-    } = UserStore()
-
-    const handleLogin = async () => {
-        setLoading(true)
-        await axiosReq.post('/auth/login', { userId, password })
-            .then((res: AxiosResponse) => {
-                resetStates()
-                notify('success', res.data?.msg)
-                setTimeout(() => {
-                    router.push('/profile')
-                }, 300)
-            }).catch((err: AxiosError) => throwError(err))
-            .finally(() => setLoading(false))
-    }
-
     return (
         <>
             <NavBar
                 pathName='login'
                 isAuthenticated={false}
             />
-            <AuthLayout
-                title='Login'
-                pathName='login'
-                btnLabel='Log In'
-                handler={handleLogin}>
-                <article className='flex flex-col gap-5'>
-                    <Input
-                        type='text'
-                        label='Email or Username'
-                        value={userId}
-                        onChange={setUserId}
-                    />
-                    <Input
-                        type='password'
-                        label='Password'
-                        value={password}
-                        placeholder="********"
-                        onChange={setPassword}
-                    />
-                </article>
-            </AuthLayout>
+            <form
+                onSubmit={(e) => e.preventDefault()}
+                className='card w-[92vw] max-w-[500px] mx-auto mt-3 mb-10 px-5 py-7'>
+                <Login title='Login' />
+            </form>
         </>
     )
 }
