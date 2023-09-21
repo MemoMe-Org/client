f688151c31fadc36eaf3a8207b9007db15cddd9f
Raheem Kawojue <kawojue08@gmail.com>
2023-09-21 10:47:08 +0100

    [edit] signin

--- lib/signin.ts
+++ lib/signin.ts
@@ -1,7 +1,17 @@
-const isProd = process.env.NODE_ENV === "production"
+import notify from '@/utils/notify'
+
+const isProd = process.env.NODE_ENV === 'production'
 
 const handleSignIn = (type: AuthMethod) => {
-    window.open(`${isProd ? process.env.NEXT_PUBLIC_AUTH_URL : 'http://localhost:2002'}/auth/${type === 'github' ? 'github' : 'google'}`, "_self")
+    const authUrlWindow = window.open(`${isProd ? process.env.NEXT_PUBLIC_AUTH_URL : 'http://localhost:2002'}/auth/${type === 'github' ? 'github' : 'google'}`, "_blank")
+
+    if (authUrlWindow) {
+        authUrlWindow.addEventListener('beforeunload', () => {
+            notify('success', 'Login successful.')
+        })
+    } else {
+        notify('error', 'Failed to login.')
+    }
 }
 
 export { handleSignIn }
