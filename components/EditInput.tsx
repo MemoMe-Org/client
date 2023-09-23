be37628cf0b3f9a8f7005ed9351d313023086075
Raheem Kawojue <kawojue08@gmail.com>
2023-09-23 01:25:42 +0100

    [add] max length

--- components/EditInput.tsx
+++ components/EditInput.tsx
@@ -2,7 +2,8 @@ import { FC } from 'react'
 
 const Input: FC<InputProps<string>> = ({
     value, label, type,
-    onChange, placeholder
+    onChange, placeholder,
+    maxLength
 }) => {
     return (
         <div className='flex items-center'>
@@ -12,12 +13,12 @@ const Input: FC<InputProps<string>> = ({
             <input
                 type={type}
                 value={value}
-                maxLength={32}
                 autoComplete='off'
                 spellCheck='false'
                 autoCorrect='false'
                 autoCapitalize='false'
                 placeholder={placeholder}
+                maxLength={maxLength || 1_000}
                 onChange={(e) => onChange(e.target.value)}
                 className='text-clr-16 rounded-r-md text-[15px] outline-none border-y-[0.5px] border-x-[0.5px] focus:border-clr-15 px-1 py-0.5'
             />
