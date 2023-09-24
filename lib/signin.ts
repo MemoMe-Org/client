ceda53ee061e64dae2fb81293322e0c506bb37ab
Raheem Muyiwa <kawojue08@gmail.com>
2023-09-24 10:20:55 +0100

    Update signin.ts

--- lib/signin.ts
+++ lib/signin.ts
@@ -2,8 +2,14 @@ import notify from '@/utils/notify'
 
 const isProd = process.env.NODE_ENV === 'production'
 
-const handleSignIn = (type: AuthMethod) => {
-    const authUrlWindow = window.open(`${isProd ? process.env.NEXT_PUBLIC_AUTH_URL : 'http://localhost:2002'}/auth/${type === 'github' ? 'github' : 'google'}`, "_blank")
+const handleSignIn = (
+    type: AuthMethod,
+    method: 'home' | 'modal'
+) => {
+    const authUrlWindow = window.open(
+    `${isProd ? process.env.NEXT_PUBLIC_AUTH_URL : 'http://localhost:2002'}/auth/${type === 'github' ? 'github' : 'google'}`,
+    `${method === 'home' ? '_self': '_blank'}`
+    )
 
     if (authUrlWindow) {
         authUrlWindow.addEventListener('beforeunload', () => {
