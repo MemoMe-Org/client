8f5692beb8bd0b9d3715b1b599f9a800cdb46b8e
Raheem Kawojue <kawojue08@gmail.com>
2023-09-25 14:58:00 +0100

    .. on poll expiry

--- components/Polls/PollExpiry.tsx
+++ components/Polls/PollExpiry.tsx
@@ -1,8 +1,42 @@
 import { FC } from 'react'
+import Modal from '../Modals/Modal'
+import { poppins } from '@/public/fonts/f'
+import { useModalStore, usePoll } from '@/utils/store'
 
-const PollExpiry: FC = () => {
+const PollExpiry: FC<{ pollId: string }> = ({ pollId }) => {
+    const { expiry, setExpiry } = usePoll()
+    const { pollExpiryModal, setPollExpiryModal } = useModalStore()
+
+    const handleExpiry = async (): Promise<void> => {
+        let date: string = ''
+
+        if (expiry) {
+            date = new Date(expiry).toISOString()
+        }
+
+        console.log(date)
+    }
+
     return (
-        <></>
+        <Modal
+            get={pollExpiryModal}
+            set={setPollExpiryModal}>
+            <h3 className={`${poppins.className} text-clr-13 text-lg font-medium`}>
+                Set Poll Expiry Date
+            </h3>
+            <article className='w-full my-3 flex justify-center items-center'>
+                <input
+                    type='datetime-local'
+                    onChange={(e) => setExpiry(e.target.value)}
+                    className={`outline-none px-1 py-0.5 rounded-md`}
+                />
+            </article>
+            <button
+                onClick={async () => handleExpiry()}
+                className='save-btn-2 absolute right-4 bottom-2'>
+                Save
+            </button>
+        </Modal>
     )
 }
 
