a47d946e319db7e3d22a000e76cc25bb14e01b42
Raheem Kawojue <kawojue08@gmail.com>
2023-09-26 02:35:14 +0100

    [add] list box

--- components/ListBox.tsx
+++ components/ListBox.tsx
@@ -1,23 +1,27 @@
-import { Fragment, useState, FC } from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+"use client"
+import { Fragment, useEffect, FC } from 'react'
 import { Listbox, Transition } from '@headlessui/react'
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 
-const genMsgs = [
-    { type: 'All' },
-    { type: 'Normal' },
-    { type: 'Relationship' },
-    { type: 'Nasty' },
-]
-
-const ListboxComponent: FC<{}> = ({ }) => {
-    const [selected, setSelected] = useState(genMsgs[0])
+const ListboxComponent: FC<ListBox> = ({
+    current, listMsgs,
+    selected, setSelected
+}) => {
+    useEffect(() => {
+        if (current) {
+            setSelected(
+                listMsgs[listMsgs.indexOf(`${current.charAt(0).toUpperCase() + current.slice(1)}`)]
+            )
+        }
+    }, [current])
 
     return (
         <div className="w-80">
             <Listbox value={selected} onChange={setSelected}>
                 <div className="relative mt-1">
                     <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-                        <span className="block truncate">{selected.type}</span>
+                        <span className="block truncate">{selected}</span>
                         <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                             <ChevronUpDownIcon
                                 className="h-5 w-5 text-gray-400"
@@ -32,21 +36,21 @@ const ListboxComponent: FC<{}> = ({ }) => {
                         leaveTo="opacity-0"
                     >
                         <Listbox.Options className="mt-1 max-h-60 w-full overflow-auto rounded-md bg-clr-0 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                            {genMsgs.map((genMsg, genMsgIdx) => (
+                            {listMsgs.map((msg, Idx) => (
                                 <Listbox.Option
-                                    key={genMsgIdx}
+                                    key={Idx}
                                     className={({ active }) =>
                                         `relative cursor-default select-none py-2 pl-10 pr-4 text-clr-0 ${active ? 'bg-clr-1' : 'text-clr-13'
                                         }`
                                     }
-                                    value={genMsg}>
+                                    value={msg}>
                                     {({ selected }) => (
                                         <>
                                             <span
                                                 className={`block truncate ${selected ? 'font-medium' : 'font-normal'
                                                     }`}
                                             >
-                                                {genMsg.type}
+                                                {msg}
                                             </span>
                                             {selected ? (
                                                 <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-clr-0">
