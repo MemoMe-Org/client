887b466f09c149e0c8e515db6271a84283bad178
Raheem Kawojue <kawojue08@gmail.com>
2023-09-26 01:19:02 +0100

    [add] poll expiry

--- components/Polls/PollExpiry.tsx
+++ components/Polls/PollExpiry.tsx
@@ -1,40 +1,69 @@
-import { FC } from 'react'
+"use client"
+import notify from '@/utils/notify'
+import axios from '@/app/api/axios'
 import Modal from '../Modals/Modal'
-import { poppins } from '@/public/fonts/f'
+import { LoaderThree } from '../Loader'
+import throwError from '@/utils/throwError'
+import { FC, useEffect, useState } from 'react'
+import { AxiosError, AxiosResponse } from 'axios'
+import { poppins, questrial } from '@/public/fonts/f'
 import { useModalStore, usePoll } from '@/utils/store'
 
 const PollExpiry: FC<{ pollId: string }> = ({ pollId }) => {
     const { expiry, setExpiry } = usePoll()
+    const [load, setLoad] = useState<boolean>(false)
+    const [defaultExpiry, setDefaultExpiry] = useState<string>()
     const { pollExpiryModal, setPollExpiryModal } = useModalStore()
 
-    const handleExpiry = async (): Promise<void> => {
-        let date: string = ''
+    useEffect(() => {
+        const currentDate = new Date()
+        currentDate.setMinutes(currentDate.getMinutes() - currentDate.getTimezoneOffset())
 
-        if (expiry) {
-            date = new Date(expiry).toISOString()
+        setDefaultExpiry(currentDate.toISOString().slice(0, 16))
+    }, [])
+
+    const handleExpiry = async (): Promise<void> => {
+        if (!expiry) {
+            notify('error', 'Invalid Date.')
+            return
         }
 
-        console.log(date)
+        setLoad(true)
+
+        const getDate = new Date(expiry)
+        if (getDate < new Date()) {
+            notify('error', 'Invalid Expiry Date.')
+            return
+        }
+
+        await axios.post(
+            `/api/poll/edit/expiry/${pollId}`,
+            { date: getDate.toISOString() }
+        )
+            .then((res: AxiosResponse) => notify('success', 'Expiry date successfully set.'))
+            .catch((err: AxiosError) => throwError(err))
+            .finally(() => setLoad(false))
     }
 
     return (
         <Modal
             get={pollExpiryModal}
             set={setPollExpiryModal}>
-            <h3 className={`${poppins.className} text-clr-13 text-lg font-medium`}>
+            <h3 className={`${poppins.className} text-clr-13 text-base font-medium`}>
                 Set Poll Expiry Date
             </h3>
             <article className='w-full my-3 flex justify-center items-center'>
                 <input
+                    value={expiry || defaultExpiry}
                     type='datetime-local'
                     onChange={(e) => setExpiry(e.target.value)}
-                    className={`outline-none px-1 py-0.5 rounded-md`}
+                    className={`${questrial.className} tracking-wider font-medium outline-none px-1 py-0.5 rounded-md text-clr-16 bg-clr-0 md:text-base text-sm`}
                 />
             </article>
             <button
                 onClick={async () => handleExpiry()}
                 className='save-btn-2 absolute right-4 bottom-2'>
-                Save
+                {load ? <LoaderThree /> : 'Save'}
             </button>
         </Modal>
     )
