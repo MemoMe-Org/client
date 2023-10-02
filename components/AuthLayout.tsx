69e303cd1c9b538e6ec0b8e93f9353fcd36bad11
Raheem Muyiwa <kawojue08@gmail.com>
2023-10-02 10:13:03 +0100

    Update AuthLayout.tsx

--- components/AuthLayout.tsx
+++ components/AuthLayout.tsx
@@ -43,13 +43,12 @@ const AuthLayout: FC<AuthProps> = ({
                         <span className="border-[1.5px] border-clr-x w-full rounded-md" />
                     </div>
                     <div className={`${lato.className} flex flex-col gap-4 my-3`}>
-                        {/* If Google verifies me - I will uncomment it. */}
-                        {/* <button
+                        <button
                             className='provider-btn'
                             onClick={() => handleSignIn('google', method!)}>
                             <FcGoogle className="text-2xl" />
                             <span>Google</span>
-                        </button> */}
+                        </button>
                         <button
                             className='provider-btn'
                             onClick={() => handleSignIn('github', method!)}>
