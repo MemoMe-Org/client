9e1f3512d4f721e44fb6e6e53040d72d9e30c480
Victor Olatunji <victoluolatunji@gmail.com>
2023-10-15 15:10:51 +0100

    cooking init

--- utils/notify.ts
+++ utils/notify.ts
@@ -1,18 +1,17 @@
-import { toast } from 'react-hot-toast'
+import { toast } from 'react-hot-toast';
 
 const notify = (action: NotifyAction, msg: string): void => {
-    if (action === 'success') {
-        toast.success(msg, {
-            duration: 3200
-        })
-    }
+  if (action === 'success') {
+    toast.success(msg, {
+      duration: 3200,
+    });
+  }
 
-    if (action === 'error') {
-        toast.error(msg, {
-            duration: 4200
-        })
-    }
-}
+  if (action === 'error') {
+    toast.error(msg, {
+      duration: 4200,
+    });
+  }
+};
 
-
-export default notify
+export default notify;
