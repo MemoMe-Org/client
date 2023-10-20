f1c5dbd60ac69f81342894ab6815fcc295281be1
Raheem Kawojue <kawojue08@gmail.com>
2023-10-20 23:21:26 +0100

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css'
+import ReactGA from 'react-ga4'
 import type { Metadata } from 'next'
 import IsAuth from '@/components/IsAuth'
 import { Toaster } from 'react-hot-toast'
-import { Analytics } from '@vercel/analytics/react'
 import QueryProvider from '@/components/QueryProvider'
 
 export const metadata: Metadata = {
@@ -10,18 +10,19 @@ export const metadata: Metadata = {
   description: 'An Ultimate Anonymous Platform for Secure Communication, Polls, and Content Control.',
 }
 
+ReactGA.initialize(process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID as string)
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className="min-h-screen">
         <Toaster
           position="top-center"
           reverseOrder={false} />
-        <Analytics />
         <QueryProvider>
           <IsAuth>
             {children}
